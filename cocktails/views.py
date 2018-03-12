d79641613961ff093c1608e4311819a8a7c69300
schodani <daniel.scholz@online.de>
2018-03-13 00:37:14 +0100

    added navbar  - now able to switch all ingredients to alcholic  - included bootstrap

--- cocktails/views.py
+++ cocktails/views.py
@@ -6,49 +6,41 @@ from .models import Cocktail, Ingredient
 
 def index(request):
     all_cocktails = Cocktail.objects.all()
-    return render(request, 'cocktails/index.html',  {"all_cocktails":all_cocktails, })
+    return render(request, 'cocktails/index.html', {"all_cocktails": all_cocktails, })
 
 
 def detail(request, id):
-    cocktail = get_object_or_404(Cocktail, pk = id)
+    cocktail = get_object_or_404(Cocktail, pk=id)
 
     try:
-        ingredients = Ingredient.objects.filter(cocktails = cocktail).all()
+        ingredients = Ingredient.objects.filter(cocktails=cocktail).all()
         print(ingredients)
 
     except:
         ingredients = ""
 
-    return render(request, 'cocktails/detail.html',  {"cocktail":cocktail, "ingredients":ingredients, })
+    return render(request, 'cocktails/detail.html', {"cocktail": cocktail, "ingredients": ingredients, })
 
 
 def favorite(request, id):
-    cocktail = get_object_or_404(Cocktail, pk = id)
+    cocktail = get_object_or_404(Cocktail, pk=id)
     try:
-        selected_ingredient = Ingredient.objects.get(pk = request.POST['ingredient'])
-        print("selected ingredient:%s" % selected_ingredient.name)
-                
-        for key in request.POST.lists():
-
-            ##################################################
-            print("%s %s" % (key.index(key[1]), " abc"))
+        selected_ingredients = []
+        for key in request.POST.getlist('ingredient'):
+            selected_ingredients.append(Ingredient.objects.get(pk=int(key)))
 
     except(KeyError, Ingredient.DoesNotExist):
-        return render(request, 'cocktails/detail.html',  {
-            "cocktail":cocktail, 
-            "error_message":"you're dumb", 
+        return render(request, 'cocktails/detail.html', {
+            "cocktail": cocktail,
+            "error_message": "you're dumb",
         })
 
-
     try:
-        ingredients = Ingredient.objects.filter(cocktails = cocktail).all()
-        print(ingredients)
+        ingredients = Ingredient.objects.filter(cocktails=cocktail).all()
 
     except:
         ingredients = ""
-
-    selected_ingredient.is_alcohol = not selected_ingredient.is_alcohol
-    selected_ingredient.save()
-    return render(request, 'cocktails/detail.html',  {"cocktail":cocktail, "ingredients":ingredients, })
-
-    
+    for s in selected_ingredients:
+        s.is_alcohol = not s.is_alcohol
+        s.save()
+    return render(request, 'cocktails/detail.html', {"cocktail": cocktail, "ingredients": ingredients, })
